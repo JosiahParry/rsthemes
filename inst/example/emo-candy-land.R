70ebe49bb71a503d964717250500271e0093e1c5
josiahparry <josiah.parry@gmail.com>
2022-04-08 14:48:48 -0400

    update

---
+++ inst/example/emo-candy-land.R
@@ -0,0 +1,25 @@
+rstheme(
+  "xxEmoCandyLandxx",
+  ui_background = "#464f57",
+  ui_foreground = rgb(247, 234, 148, maxColorValue = 255),
+  code_string = rgb(254, 158, 229, maxColorValue = 255),
+  code_value = "#DA71A3",
+  code_operator = "#FF6B6B", # operators are pipes etc
+  code_variable = "#FFDF7F",
+  code_message = "#EB994C",
+  ui_selection = "#C4832178", # highlight color
+  ui_gutter_background = "#372b2b3d",
+  ui_gutter_foreground = "#a36666",
+  code_comment = "#C38484", # maroonish
+  code_comment_background = rgb(255, 255, 255, 5, maxColorValue = 255),
+  code_function = "#F77171",
+  theme_dark = TRUE,
+  theme_flat = FALSE,
+  # used for rmd links or something like that
+  code_reserved = rgb(255, 255, 255, 200, maxColorValue = 255),
+  "$terminal_color_blue: #81d497;",
+  "$terminal_color_red: #ff6161;",
+  "$terminal_color_black_bright: #91927d;",
+  "$terminal_color_magenta: #b384bb;",
+  theme_path = "emo-candyland.rstheme"
+)
